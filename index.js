2657bf659e18e69f5c452177fd6d4fbfd43e3e60
Dmitry Shamrilo <shamrilods@gmail.com>
2020-10-18 21:07:22 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
+
+const STATIC_PORT = 3000
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Home Page Route"));
+app.get('/', (req, res) => res.send('Home Page Route'));
 
-app.get("/about", (req, res) => res.send("About Page Route"));
+app.get('/about', (req, res) => res.send('About Page Route'));
 
-app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.get("/contact", (req, res) => res.send("Contact Page Route"));
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || STATIC_PORT;
 
 app.listen(port, () =>
   console.log(`Server running on ${port}, http://localhost:${port}`)
